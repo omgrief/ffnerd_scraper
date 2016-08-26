23824a2465458c215fc9f00ae4d6ce2cea5f0a16
Robert <robertbobhuang@Gmail.com>
2016-08-26 01:17:28 -0700

    Endpoints

--- src/endpoints/v1/index.js
+++ src/endpoints/v1/index.js
@@ -1,5 +1,25 @@
 'use strict';
-
+const debug = require('abacus-debug')(process.env.APP_NAME + '-endpoints');
+const _ = require('lodash');
 module.exports = (config) => {
+  const router = require('express').Router();
 
+  const handlers = require('../../handlers/v1')(config);
+
+  const jsonParser = require('body-parser').json();
+
+  const routes = require('./routes');
+
+  routes.forEach((definition) => {
+    const path = definition.path;
+    // Replace / with _
+    const _path = path.replace(/^\//, '').split('/').join('_');
+    definition.verbs.forEach((verb) => {
+      router[verb](
+        path,
+        jsonParser,
+        handlers.gm(dotpath, verb)
+      );
+    });
+  });
 }
---
+++ src/endpoints/v1/routes.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {
+    path: '/players',
+    verbs: ['GET']
+  }
+]
